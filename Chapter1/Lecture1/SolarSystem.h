de447ef4829df85ddee7fa2ed3f86b7ca31b0084
jeongmin94 <extraking@naver.com>
2023-05-05 21:01:43 +0900

    feat: add solar system

--- Chapter1/Lecture1/SolarSystem.h
+++ Chapter1/Lecture1/SolarSystem.h
@@ -2,29 +2,25 @@
 
 #include "Game2D.h"
 
-namespace jm
-{
-	class SolarSystem : public Game2D
-	{
-		float time = 0.0f;
+namespace jm {
+class SolarSystem : public Game2D {
+    float time = 0.0f;
 
-	public:
-		void update() override
-		{
-			beginTransformation();
-			{
-				drawFilledStar(Colors::gold, 0.2f, 0.13f);	// Sun
+public:
+    void update() override
+    {
+        rotate(time * 60.0f);
+        drawFilledStar(Colors::gold, 0.2f, 0.13f); // Sun
 
-				translate(0.5f, 0.0f);
-				drawFilledCircle(Colors::blue, 0.1f);		// Earth
+        rotate(time * 60.0f * 1.5f);
+        translate(0.5f, 0.0f);
+        drawFilledCircle(Colors::blue, 0.1f); // Earth
 
-				translate(0.2f, 0.0f);
-				drawFilledCircle(Colors::yellow, 0.05f);	// Moon
-			}
-			endTransformation();
+        rotate(time * 150.0f);
+        translate(0.2f, 0.0f);
+        drawFilledCircle(Colors::yellow, 0.05f); // Moon
 
-			time += this->getTimeStep();
-
-		}
-	};
-}
+        time += this->getTimeStep();
+    }
+};
+} // namespace jm
--- Chapter1/Lecture1/TransformationExample.cpp
+++ Chapter1/Lecture1/TransformationExample.cpp
@@ -1,14 +1,23 @@
 #include "TransformationExample.h"
 
 void jm::TransformationExample::rotateBox() {
-    const vec2 center(-0.2f, 0.0f);
+    const vec2 center(0.0f, 0.0f);
 
-	rotate(time * 90.0f);
-	translate(-center);
-	rotate(time * 90.0f);
-	scale(2.0f, 0.2f);
-	drawPoint(Colors::black, center, 5.0f);
-    drawWiredBox(Colors::red, 0.4f, 0.2f);
+    beginTransformation();
+    {
+        rotate(time * 60.0f);
+	    translate(0.5f, 0.0f);
+        drawWiredBox(Colors::red, 1.0f, 0.2f);
+
+        beginTransformation();
+        {
+            rotate(time*90.0f);
+            drawWiredBox(Colors::blue, 0.5f, 0.2f);
+        }
+        endTransformation();
+    }
+    endTransformation();
+
 
 	time += getTimeStep();
 }
