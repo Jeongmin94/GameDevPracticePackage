050ce7b39018ee0d25f71c2aaaec37986a247187
jeongmin94 <extraking@naver.com>
2023-05-13 19:49:15 +0900

    feat: atan2를 이용한 각도 구하기 및 회전 구현

--- Chapter1/Lecture1/Follow.h
+++ Chapter1/Lecture1/Follow.h
@@ -5,21 +5,34 @@
 namespace jm {
 class Follow : public Game2D {
 private:
-    vec2 center = vec2(0.0f, 0.0f);
-    void drawDirection(const vec2 &cursor)
-    {
-        //translate(center);
+    const vec2 gun_pos;
 
-        float d = atan2f(cursor.y, cursor.x);
-        rotate(180 / 3.141592f * (d));
-        drawFilledBox(Colors::yellow, 0.4f, 0.02f);
+    float getDegree(const float &radian) { return 180 / 3.141592f * radian; }
+
+    void drawCenter() { drawFilledCircle(Colors::yellow, 0.2f); }
+
+    void drawGun() { drawFilledBox(Colors::black, 0.3f, 0.075f); }
+
+    void drawTurret(const vec2 &cursor)
+    {
+        rotate(getDegree(atan2(cursor.y, cursor.x)));
+        drawCenter();
+
+        translate(gun_pos);
+        drawGun();
     }
 
 public:
+
+    Follow() : gun_pos(vec2(0.3f, 0.0f))
+    {
+
+    }
+
     void update() override
     {
         vec2 cursor = getCursorPos(true);
-        drawDirection(cursor);
+        drawTurret(cursor);
     }
 };
 } // namespace jm
