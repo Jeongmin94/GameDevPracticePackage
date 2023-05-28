af2018f1c97d3c861b309776d895571e6f2c07d5
jeongmin94 <extraking@naver.com>
2023-05-28 14:35:18 +0900

    feat: stream overloading을 사용하여 Exam 객체 생성

--- Chapter2/Lecture4/GeometricObject.h
+++ Chapter2/Lecture4/GeometricObject.h
@@ -2,33 +2,31 @@
 
 #include "Game2D.h"
 
-namespace jm
-{
-	class GeometricObject
-	{
-	public:
-		vec2 pos;
-		RGB  color;
+namespace jm {
+class GeometricObject {
+public:
+    vec2 pos;
+    RGB color;
 
-		//virtual ~GeometricObject()
-		//{}
+    // virtual ~GeometricObject()
+    //{}
 
-		void init(const RGB & _color, const vec2 & _pos)
-		{
-			color = _color;
-			pos = _pos;
-		}
+    void init(const RGB &_color, const vec2 &_pos)
+    {
+        color = _color;
+        pos = _pos;
+    }
 
-		virtual void drawGeometry() const = 0;
+    virtual void drawGeometry() const = 0;
 
-		void draw()
-		{
-			beginTransformation();
-			{
-				translate(pos);
-				drawGeometry();
-			}
-			endTransformation();
-		}
-	};
-}
+    void draw()
+    {
+        beginTransformation();
+        {
+            translate(pos);
+            drawGeometry();
+        }
+        endTransformation();
+    }
+};
+} // namespace jm
--- Chapter2/Lecture4/Triangle.h
+++ Chapter2/Lecture4/Triangle.h
@@ -2,32 +2,25 @@
 
 #include "GeometricObject.h"
 
-namespace jm
-{
-	class Triangle : public GeometricObject
-	{
-	public:
-		float size;
+namespace jm {
+class Triangle : public GeometricObject {
+public:
+    float size;
 
-		Triangle(const RGB & _color, const vec2 & _pos, const float & _size)
-		{
-			init(_color, _pos, _size);
-		}
+    Triangle(const RGB &_color, const vec2 &_pos, const float &_size)
+    {
+        printf("Triangle init\n");
+        init(_color, _pos, _size);
+    }
 
-		~Triangle()
-		{
-			printf("Triangle destructor\n");
-		}
+    ~Triangle() { printf("Triangle destructor\n"); }
 
-		void init(const RGB & _color, const vec2 & _pos, const float & _size)
-		{
-			GeometricObject::init(_color, _pos);
-			size = _size;
-		}
+    void init(const RGB &_color, const vec2 &_pos, const float &_size)
+    {
+        GeometricObject::init(_color, _pos);
+        size = _size;
+    }
 
-		void drawGeometry() const override
-		{
-			drawFilledTriangle(color, size);
-		}
-	};
-}
+    void drawGeometry() const override { drawFilledTriangle(color, size); }
+};
+} // namespace jm
