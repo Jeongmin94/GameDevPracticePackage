bc294944631bb381f4ce0dbeab78c80c00ea75d9
jeongmin <extraking@naver.com>
2023-06-03 23:25:53 +0900

    feat: 질량 용수철 시스템 연습문제 추가

--- Chapter3/Lecture3/RigidCircle.h
+++ Chapter3/Lecture3/RigidCircle.h
@@ -2,66 +2,84 @@
 
 #include "Game2D.h"
 
-namespace jm
-{
-	class RigidCircle
-	{
-	public:
-		vec2 pos;
-		vec2 vel;
-		RGB color = Colors::hotpink;
-		float radius = 0.1f;
-		float mass = 1.0f;
+namespace jm {
+class RigidCircle {
+public:
+    vec2 pos;
+    vec2 vel;
+    RGB color = Colors::hotpink;
+    float radius = 0.1f;
+    float mass = 1.0f;
 
-	public:
-		void draw()
-		{
-			beginTransformation();
-			{
-				translate(pos);
-				drawFilledCircle(color, radius - 1e-3f);
-				setLineWidth(2.0f);
-				drawWiredCircle(Colors::black, radius);
-			}
-			endTransformation();
-		}
+public:
+    RigidCircle() {}
 
-		void update(const float & dt)
-		{
-			static const vec2 gravity = vec2(0.0f, -9.8f);
-			static const float coef_res = 0.7f; // coefficient of restitution
-			static const float coef_friction = 0.99f; // friction (not physical)
+    RigidCircle(const vec2 &pos, const vec2 &vel, const RGB &color,
+                const float &radius, const float &mass)
+        : pos(pos), vel(vel), color(color), radius(radius), mass(mass)
+    {
+    }
 
-			// numerical integration
-			vel += gravity * dt;
-			pos += vel * dt;
+    void init(const vec2 &_pos, const vec2 &_vel, const RGB &_color,
+              const float &_radius, const float &_mass)
+    {
+        pos = _pos;
+        vel = _vel;
+        color = _color;
+        radius = _radius;
+        mass = _mass;
+    }
 
-			// wall collision, friction
-			if (1.0f - pos.x <= radius) // right wall
-			{
-				pos.x = 1.0f - radius;
+    void draw()
+    {
+        beginTransformation();
+        {
+            translate(pos);
+            drawFilledCircle(color, radius - 1e-3f);
+            setLineWidth(2.0f);
+            drawWiredCircle(Colors::black, radius);
+        }
+        endTransformation();
+    }
 
-				if (vel.x >= 0.0f)
-					vel.x *= -1.0f * coef_res;
-			}
+    void update(const float &dt)
+    {
+        static const vec2 gravity = vec2(0.0f, -9.8f);
+        static const float coef_res = 0.7f;       // coefficient of restitution
+        static const float coef_friction = 0.99f; // friction (not physical)
 
-			if (pos.x <= -1.0f + radius) // left wall
-			{
-				pos.x = -1.0f + radius;
+        // numerical integration
+        vel += gravity * dt;
+        pos += vel * dt;
 
-				if (vel.x <= 0.0f)
-					vel.x *= -1.0f * coef_res;
-			}
+        // wall collision, friction
+        if (1.0f - pos.x <= radius) // right wall
+        {
+            pos.x = 1.0f - radius;
 
-			if (pos.y <= -1.0f + radius) // ground
-			{
-				pos.y = -1.0f + radius;
+            if (vel.x >= 0.0f)
+                vel.x *= -1.0f * coef_res;
+        }
 
-				if (vel.y <= 0.0f)
-					vel.y *= -1.0f * coef_res;
+        if (pos.x <= -1.0f + radius) // left wall
+        {
+            pos.x = -1.0f + radius;
 
-				vel.x *= coef_friction;
-			}
-		}
-	};
-}
+            if (vel.x <= 0.0f)
+                vel.x *= -1.0f * coef_res;
+        }
+
+        if (pos.y <= -1.0f + radius) // ground
+        {
+            pos.y = -1.0f + radius;
+
+            if (vel.y <= 0.0f)
+                vel.y *= -1.0f * coef_res;
+
+            vel.x *= coef_friction;
+        }
+    }
+
+    ~RigidCircle() { std::cout << "destructor!!!" << std::endl; }
+};
+} // namespace jm
